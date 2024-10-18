a23b511a27bcd0170afe0b538348b6d00f4f8d95
Deivis <rumsasdg@gmail.com>
2024-10-18 15:00:48 +0300

    Add theme switch and mobile dropdown

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,12 +3,20 @@ import Link from "next/link";
 import { ReactNode } from "react";
 import Logo from "@/public/logo.png";
 import { DashboardLinks } from "../components/DashboardLinks";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { auth } from "../lib/auth";
 
-export default function DashboardLayout({children}: {children: ReactNode}) {
+export default async function DashboardLayout({children}: {children: ReactNode}) {
+    const session = await auth()
     return (
         <>
             <div className="min-h-screen w-full grid md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]" > 
-                <div className="hidden md:block border-r bg-muted/40">
+                {/* Side bar */}
+                <div className="hidden md:block border-r bg-muted/40"> 
                     <div className="flex h-full max-h-screen flex-col gap-2">
                         <div className="flex h-14 items-center border-b px-4 lg:h[60px] lg:px-6">
                             <Link href="/" className="flex items-center gap-2">
@@ -22,6 +30,33 @@ export default function DashboardLayout({children}: {children: ReactNode}) {
                             </nav>
                         </div>
                     </div>
+                </div>
+                {/* Top bar */}
+                <div className="flex flex-col">
+                    <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+                        <Sheet>
+                            <SheetTrigger asChild>
+                                <Button className="md:hidden shrink-0" size="icon" variant="outline">
+                                    <Menu className="size-5" />
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent side="left" className="flex flex-col">
+                                <nav className="grid gap-2 mt-10">
+                                    <DashboardLinks />
+                                </nav>
+                            </SheetContent>
+                        </Sheet>
+                        <div className="ml-auto flex items-center gap-x-4">
+                            <ThemeToggle />
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button size="icon" variant="outline">
+                                        <img src={session?.user?.image as string} alt="Profile Image" className="size-8 rounded-full" />
+                                    </Button>
+                                </DropdownMenuTrigger>
+                            </DropdownMenu>
+                        </div>
+                    </header>
                 </div>
             </div>
         </>
