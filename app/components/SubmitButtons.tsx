cf7965832a07ded3cb31519262d5280ce3026c1b
Deivis <rumsasdg@gmail.com>
2024-10-19 03:37:33 +0300

    Create onboarding flow with serverside and clientside validation

--- app/components/SubmitButtons.tsx
+++ app/components/SubmitButtons.tsx
@@ -5,6 +5,39 @@ import { useFormStatus } from "react-dom";
 import GoogleLogo from "@/public/google.svg";
 import GithubLogo from "@/public/github.svg";
 import { Loader2 } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface iAppProps {
+    text: string;
+    variant?: 
+    | "default" 
+    | "destructive" 
+    | "outline" 
+    | "secondary" 
+    | "ghost" 
+    | "link" 
+    | null 
+    | undefined;
+
+    className?: string;
+}
+
+export function SubmitButton({text, variant, className}: iAppProps) {
+    const {pending} = useFormStatus ()
+    return (
+        <>
+        {pending ? (
+            <Button disabled variant="outline" className={cn("w-fit", className)}>
+                <Loader2 className="mr-2 size-4 animate-spin" /> Loading...
+            </Button>
+        ) : (
+            <Button  type="submit" variant={variant} className={cn("w-fit", className)}>
+                {text}
+            </Button>
+        )}
+        </>
+    );
+}
 
 export function GoogleAuthButton() {
     const {pending} = useFormStatus ()
---
+++ app/onboarding/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useFormState } from "react-dom";
+import { OnboardingAction } from "../actions";
+import { useForm } from '@conform-to/react'
+import { parseWithZod } from "@conform-to/zod";
+import { onboardingSchema } from "../lib/zodSchemas";
+import { SubmitButton } from "../components/SubmitButtons";
+
+export default function OnboardingRoute() {
+    const [lastResult, action] = useFormState(OnboardingAction, undefined);
+
+    const [ form, fields ] = useForm({
+        lastResult,
+        onValidate({formData}) {
+            return parseWithZod(formData, { 
+                schema: onboardingSchema })
+        },
+        shouldValidate: 'onBlur',
+        shouldRevalidate: 'onInput',
+    })
+    return (
+        <div className="min-h-screen w-screen flex items-center justify-center">
+            <Card>
+                <CardHeader>
+                    <CardTitle>Welcome to Cal<span className="text-primary">App</span></CardTitle>
+                    <CardDescription>We need a few details to get you setup.</CardDescription>
+                </CardHeader>
+                <form id={form.id} onSubmit={form.onSubmit} action={action} noValidate>
+                <CardContent className="grid gap-y-5">
+                    <div className="grid gap-y-2">
+                        <Label>Full Name</Label>
+                        <Input name={fields.fullName.name} defaultValue={fields.fullName.initialValue} key={fields.fullName.key} placeholder="John Doe" />
+                        <p className="text-sm text-red-500">{fields.fullName.errors}</p>
+                    </div>
+                    <div className="grid gap-y-2">
+                        <Label>Username</Label>
+                        <div className="flex rounded-md">
+                            <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-muted bg-muted text-sm text-muted-foreground">CalApp.com/</span>
+                            <Input name={fields.userName.name} defaultValue={fields.userName.initialValue} key={fields.userName.key} placeholder="johndoe" className="rounded-l-none" />
+                        </div>
+                        <p className="text-sm text-red-500">{fields.userName.errors}</p>
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <SubmitButton text="Submit" className="w-full"/>
+                </CardFooter>
+                </form>
+            </Card>
+        </div>
+    )
+}
